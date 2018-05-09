41fb94311a0f0b3550944e16389337c48fbc0b4e
Rasmus H. Hummelmose <rasmus@hummelmose.dk>
2018-05-09 20:06:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Client IP Logger
+
+For testing application gateway :)
---
+++ index.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+
+    var clientIPAddress = request.connection.remoteAddress;
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hi, you're on ip: %s", clientIPAddress);
+
+});
+
+var port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
